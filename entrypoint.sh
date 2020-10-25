d7e1e7c5e819b8ea25c7bd5e81e3dff2400b6317
Mohamed Jasim Ahmed <34663944+MJAISE@users.noreply.github.com>
2020-10-25 20:25:16 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,7 +26,7 @@ then
 	ssh-keyscan -p $SSH_PORT -H "$SSH_HOST" >> /root/.ssh/known_hosts
 fi
 
-rsync --progress -azh \
+rsync --progress -avzh \
 	--exclude='.git/' \
 	--exclude='.git*' \
 	--exclude='.editorconfig' \
