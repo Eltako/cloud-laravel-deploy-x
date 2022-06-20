e5bf51f4eeb89227949f665bdcc9af98be1040a9
Christoph Mertens <christoph.mertens@eltako.de>
2022-06-20 14:54:51 +0200

    Running artisan only if parameter is set...

--- entrypoint.sh
+++ entrypoint.sh
@@ -51,9 +51,9 @@ then
 	# ssh -i /root/.ssh/id_rsa -t $SSH_USER@$SSH_HOST "sudo chmod 777 -R $PATH_SOURCE/storage"
 	# ssh -i /root/.ssh/id_rsa -t $SSH_USER@$SSH_HOST "sudo chmod 777 -R $PATH_SOURCE/public"
 
-	if []
+	if ["$USE_ARTISAN" == '1']
 	then
-	ssh -i /root/.ssh/id_rsa -t $SSH_USER@$SSH_HOST "cd $PATH_SOURCE && php artisan migrate && php artisan cache:clear && php artisan route:cache && php artisan config:cache"
+		ssh -i /root/.ssh/id_rsa -t $SSH_USER@$SSH_HOST "cd $PATH_SOURCE && php artisan migrate && php artisan cache:clear && php artisan route:cache && php artisan config:cache"
 	fi
 
 	if [ ! -z "$COMMANDS" ];
